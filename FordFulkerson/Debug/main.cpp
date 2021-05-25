122f37cdc6cf598e6655a032463bddf8211285b2
Klementina424 <58881767+Klementina424@users.noreply.github.com>
2021-05-26 00:21:09 +0300

    Add files via upload

---
+++ FordFulkerson/Debug/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "FordFulkerson.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello, you are welcome!\n";
+    cout << "Please, input the filename: ";
+    string filename;
+    //filename = "test.txt";
+    cin >> filename;
+    FordFulkerson alg;
+    alg.inputData(filename);
+    int res = alg.MaxStream();
+    cout <<"Answer:" << res;
+}
